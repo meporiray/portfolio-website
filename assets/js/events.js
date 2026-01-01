9f203dbeced6ff309b6e50d97e4f255e1e69a38c
Paritosh Chandra Ray <meporiray@gmail.com>
2026-01-02 02:41:28 +0600

    Add AOS initialization for animations on load

---
+++ assets/js/events.js
@@ -0,0 +1,10 @@
+// Initialize AOS Animations
+
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 1000,
+        once: true,
+        mirror: false,
+        offset: 100
+    });
+});
