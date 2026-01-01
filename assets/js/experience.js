8fe55556542fe5a396f48131cef624a66d9bafa9
Paritosh Chandra Ray <meporiray@gmail.com>
2026-01-02 01:48:26 +0600

    Initialize AOS animations on DOMContentLoaded

---
+++ assets/js/experience.js
@@ -0,0 +1,10 @@
+// Initialize AOS Animations
+
+document.addEventListener('DOMContentLoaded', function() {
+AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+    });
+});
